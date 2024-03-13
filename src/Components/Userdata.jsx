9cc87f9c6e0151cd59869aaa26f2a9e8535f1ac2
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-03-13 12:24:29 +0530

    commit to understand table tag in html better

--- src/Components/Userdata.jsx
+++ src/Components/Userdata.jsx
@@ -3,15 +3,25 @@ const Userdata = ({ states }) => {
   return (
     <>
       {states.map((elm, i) => {
-        const { id, name, username, email, website} = elm;
+        const { id, name, username, email, website } = elm;
         const { street, city, zipcode } = elm.address;
         return (
           <tr key={i} /* table row */>
-            <td className="p-2 border-2 border-red-600" /* tbody row cell */>{id}</td>
-            <td className="p-2 border-2 border-red-600" /* tbody row cell */>{name}</td>
-            <td className="p-2 border-2 border-red-600" /* tbody row cell */>{username}</td>
-            <td className="p-2 border-2 border-red-600" /* tbody row cell */>{email}</td>
-            <td className="p-2 border-2 border-red-600" /* tbody row cell */>{website}</td>
+            <td className="p-2 border-2 border-red-600" /* tbody row cell */>
+              {id}
+            </td>
+            <td className="p-2 border-2 border-red-600" /* tbody row cell */>
+              {name}
+            </td>
+            <td className="p-2 border-2 border-red-600" /* tbody row cell */>
+              {username}
+            </td>
+            <td className="p-2 border-2 border-red-600" /* tbody row cell */>
+              {email}
+            </td>
+            <td className="p-2 border-2 border-red-600" /* tbody row cell */>
+              {website}
+            </td>
             <td className="p-2 border-2 border-red-600" /* tbody row cell */>
               {street},{city},{zipcode}
             </td>
